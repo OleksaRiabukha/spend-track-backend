2d69e1ebf13bd9c207350cf9b0859c679ccc09d1
A_Wheatman <oleksa.wheat@gmail.com>
2021-06-23 09:54:43 +0300

    Add User controller with create method and routes for POST request

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+  
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user, status: :ok
+    else
+      render json: { errors: user.errors }, status: :bad_request
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
+  end
+end
