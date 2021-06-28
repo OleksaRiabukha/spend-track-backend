1209b31f0e7ac7c719de77e00b8718ee5a1bfd70
A_Wheatman <oleksa.wheat@gmail.com>
2021-06-28 14:51:26 +0300

    Fix for deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   before_action :authorized
 
   def jwt_key
-    Rails.application.credentials.jwt_key
+    ENV['SESSION_SECRET']
   end
 
   def issue_token(user)
